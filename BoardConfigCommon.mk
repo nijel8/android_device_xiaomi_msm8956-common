7a27b3e8ee0915232c8445615f15eddf17bc5248
nijel8 <nijel8@gmail.com>
2017-01-17 19:54:24 -0500

    Getting rid of that generic thing...
    Change-Id: Id83bc3e453f651efcc153c85e2c276dfb76e1226

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,15 +18,12 @@ VENDOR_PATH := device/xiaomi/msm8956-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
 
-# Use Snapdragon LLVM, if available
-TARGET_USE_SDCLANG := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
