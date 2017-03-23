aae7fab79b28e9df20911ed79864b3a714c42ece
nijel8 <nijel8@gmail.com>
2017-03-23 00:01:55 -0400

    We are not using Qualcomm TimeService

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -194,7 +194,6 @@ TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QC_TIME_SERVICES := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/etc/fstab.qcom
