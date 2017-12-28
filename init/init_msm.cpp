c409d6c0fb210e7004d61ba625fd7e0abfc764d6
nijel8 <nijel8@gmail.com>
2017-12-28 17:56:30 -0500

    init: Add ro.alarm_boot property support
    *ro.alarm_boot property is used to let framework detect if it
    is RTC triggered boot.

--- init/init_msm.cpp
+++ init/init_msm.cpp
@@ -28,6 +28,14 @@
 #include <sys/sysinfo.h>
 
 #include "property_service.h"
+#include "util.h"
+
+#include <android-base/strings.h>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+using android::base::Trim;
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
@@ -35,12 +43,58 @@ char const *heapsize;
 char const *heapminfree;
 char const *heapmaxfree;
 
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+static void init_alarm_boot_properties()
+{
+    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
+    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
+    std::string boot_reason;
+    std::string power_off_alarm;
+    std::string reboot_reason = property_get("ro.boot.alarmboot");
+
+    if (read_file(boot_reason_file, &boot_reason)
+                 && read_file(power_off_alarm_file, &power_off_alarm))
+    {
+        /*
+        * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+        * For existing PMIC chips, the following mapping applies
+        * for the value of boot_reason:
+        * 0 -> unknown
+        * 1 -> hard reset
+        * 2 -> sudden momentary power loss (SMPL)
+        * 3 -> real time clock (RTC)
+        * 4 -> DC charger inserted
+        * 5 -> USB charger inserted
+        * 6 -> PON1 pin toggled (for secondary PMICs)
+        * 7 -> CBLPWR_N pin toggled (for external power supply)
+        * 8 -> KPDPWR_N pin toggled (power key pressed)
+        */
+        if ((Trim(boot_reason) == "3" || reboot_reason == "true")
+                 && Trim(power_off_alarm) == "1")
+        {
+            property_override("ro.alarm_boot", "true");
+        } else {
+            property_override("ro.alarm_boot", "false");
+        }
+    }
+}
+
 void check_device()
 {
     struct sysinfo sys;
 
     sysinfo(&sys);
-    
+
     if (sys.totalram > 3072ull * 1024 * 1024)
     {
         // from - phone-xxhdpi-4096-dalvik-heap.mk
@@ -94,4 +148,6 @@ void vendor_load_properties()
     {
         property_set("persist.sys.cpu.core_ctl", "0");
     }
+
+    init_alarm_boot_properties();
 }
