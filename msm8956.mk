324ee067338e5405f5558e8c399c3dec83926059
nijel8 <nijel8@gmail.com>
2018-01-23 03:11:08 -0500

    SU: Commonize build

--- msm8956.mk
+++ msm8956.mk
@@ -272,6 +272,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9335.xml:system/etc/sound_trigger_mixer_paths_wcd9335.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# SU
+WITH_SU := true
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8956.conf:system/etc/thermal-engine-8956.conf \
