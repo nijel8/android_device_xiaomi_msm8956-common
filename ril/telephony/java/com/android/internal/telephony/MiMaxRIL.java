12186e654e4194af79c234b2934b1817ab4c6d6e
nijel8 <nijel8@gmail.com>
2018-01-23 03:09:54 -0500

    RIL: Fix import/export contacts from/to SIM card
    *** requires branch cm-14.0-caf RIL ***
    Add these lines to your roomservice.xml:
    
    <remove-project name="LineageOS/android_hardware_ril" />
    <project path="hardware/ril-caf" name="LineageOS/android_hardware_ril" groups="pdk" revision="cm-14.0-caf" />
    
    then repo sync...

--- ril/telephony/java/com/android/internal/telephony/MiMaxRIL.java
+++ ril/telephony/java/com/android/internal/telephony/MiMaxRIL.java
@@ -18,15 +18,25 @@ package com.android.internal.telephony;
 
 import android.content.Context;
 import android.os.AsyncResult;
+import android.os.Message;
 import android.os.Parcel;
-
 import android.os.SystemProperties;
+import android.service.carrier.CarrierIdentifier;
 import android.util.Log;
 
 import static com.android.internal.telephony.RILConstants.*;
 
+import java.util.List;
+
 /**
  * Custom Qualcomm RIL for Xiaomi Mi Max/Prime
+ * *************************************************************************
+ * Remap Android 7.1 RILConstants to match Xiaomi's Android 7.0 RIL
+ * Note: Build with matching cm-14.0-caf RIL
+ * Fixes import/export contacts from/to SIM card
+ * -------------------------------------------------------------------------
+ * Remap unsupported DC-HSPAP RAT=20 to HSPAP RAT=15
+ * Fixes missing H+ status bar icon for RAT=20 use cases
  *
  * {@hide}
  */
@@ -46,6 +56,33 @@ public class MiMaxRIL extends RIL implements CommandsInterface {
     }
 
     @Override
+    protected void
+    send(RILRequest rr) {
+        int origRequest = rr.mRequest;
+        switch(origRequest) {
+            case RIL_REQUEST_SIM_GET_ATR:
+            case RIL_REQUEST_CAF_SIM_OPEN_CHANNEL_WITH_P2:
+            case RIL_REQUEST_GET_ADN_RECORD:
+            case RIL_REQUEST_UPDATE_ADN_RECORD:
+                rr.mRequest -= 2; // adjust for our RIL
+                break;
+        }
+
+        if (origRequest != rr.mRequest) {
+            int dataPosition = rr.mParcel.dataPosition();
+            rr.mParcel.setDataPosition(0);
+            rr.mParcel.writeInt(rr.mRequest);
+            rr.mParcel.setDataPosition(dataPosition);
+
+            if (DEBUG) Log.d(TAG, "send: Remapped request before sending from " +
+                       requestToString(origRequest) + "[" + origRequest + "] to " +
+                       requestToString(rr.mRequest) + "[" + rr.mRequest + "]");
+        }
+
+        super.send(rr);
+    }
+
+    @Override
     protected RILRequest
     processSolicited (Parcel p, int type) {
         int serial, error;
@@ -53,11 +90,14 @@ public class MiMaxRIL extends RIL implements CommandsInterface {
         int dataPosition = p.dataPosition(); // Save off position within the parcel
         serial = p.readInt();
         error = p.readInt();
+        int origRequest = -1;
         RILRequest rr = null;
+        RILRequest tr = null;
 
         // Pre-process the reply before popping it
         synchronized (mRequestList) {
-            RILRequest tr = mRequestList.get(serial);
+            tr = mRequestList.get(serial);
+            origRequest = tr.mRequest;
             if (tr != null && tr.mSerial == serial) {
                 if (error == 0 || p.dataAvail() > 0) {
                     try {
@@ -65,6 +105,12 @@ public class MiMaxRIL extends RIL implements CommandsInterface {
                             // Get those we're interested in
                             case RIL_REQUEST_DATA_REGISTRATION_STATE:
                                 rr = tr;
+                                break;
+                            case RIL_REQUEST_SET_ALLOWED_CARRIERS:
+                            case RIL_REQUEST_GET_ALLOWED_CARRIERS:
+                            case RIL_REQUEST_SIM_GET_ATR:
+                            case RIL_REQUEST_CAF_SIM_OPEN_CHANNEL_WITH_P2:
+                                tr.mRequest += 2; // revert adjust for our RIL
                                 break;
                         }
                     } catch (Throwable thr) {
@@ -77,6 +123,17 @@ public class MiMaxRIL extends RIL implements CommandsInterface {
                     }
                 }
             }
+        }
+
+        if (origRequest != -1 && origRequest != tr.mRequest) {
+            // revert back request remapping in mRequestList before
+            // forwarding it to the super class for response processing
+            mRequestList.remove(serial);
+            mRequestList.append(serial, tr);
+
+            if (DEBUG) Log.d(TAG, "processSolicited: Reverting remapped request response before processing from " +
+                       requestToString(origRequest) + "[" + origRequest + "] to " +
+                       requestToString(tr.mRequest) + "[" + tr.mRequest + "]");
         }
 
         if (rr == null) {
@@ -127,5 +184,58 @@ public class MiMaxRIL extends RIL implements CommandsInterface {
 
         return response;
     }
-}
 
+    @Override
+    protected void
+    processUnsolicited(Parcel p, int type) {
+        int dataPosition = p.dataPosition();
+        int origResponse = p.readInt();
+        int newResponse = origResponse;
+        try {
+            switch(origResponse) {
+                case RIL_UNSOL_PCO_DATA:
+                case RIL_UNSOL_RESPONSE_ADN_INIT_DONE:
+                    newResponse += 1; // adjust Uncol response for processing
+                    break;
+            }
+        } catch (Throwable tr) {
+            Log.e(TAG, "Exception processing unsol response: " + origResponse +
+                "Exception:" + tr.toString());
+            return;
+        }
+
+        if (newResponse != origResponse) {
+            p.setDataPosition(dataPosition);
+            p.writeInt(newResponse);
+
+            if (DEBUG) Log.d(TAG, "processUnsolicited: Remapped Uncol response from " +
+                       responseToString(origResponse) + "[" + origResponse + "] to " +
+                       responseToString(newResponse) + "[" +  newResponse + "]" +
+                       " before processing");
+        }
+
+        p.setDataPosition(dataPosition);
+        super.processUnsolicited(p, type);
+    }
+
+    // Our RIL doesn't support these...
+    @Override
+    public void setAllowedCarriers(List<CarrierIdentifier> carriers, Message response) {
+        if (DEBUG) Log.d(TAG, "setAllowedCarriers: not supported");
+        if (response != null) {
+            CommandException ex = new CommandException(CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response, null, ex);
+            response.sendToTarget();
+        }
+    }
+
+    @Override
+    public void getAllowedCarriers(Message response) {
+        if (DEBUG) Log.d(TAG, "getAllowedCarriers: not supported");
+        if (response != null) {
+            CommandException ex = new CommandException(CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response, null, ex);
+            response.sendToTarget();
+        }
+    }
+}
