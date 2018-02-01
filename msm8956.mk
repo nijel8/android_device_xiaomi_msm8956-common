98c36e25f161f8334bf6dd129757224881cc3f89
nijel8 <nijel8@gmail.com>
2018-02-01 14:37:11 -0500

    Remove Battery usage alerts feature
    *Who cares...

--- msm8956.mk
+++ msm8956.mk
@@ -62,10 +62,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-# Android 7.1.2 "Battery Usage Alerts" feature (apk from OpenGapps)
-PRODUCT_PACKAGES += \
-    Turbo
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
